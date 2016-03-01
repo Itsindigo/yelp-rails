cf361dade4ccba02a2ff963f9e1004eb2cc076a1
itsindigo <matthewbridgesbusiness@gmail.com>
2016-03-01 11:22:59 +0000

    Completed CRUD for Restaurant, created reviewd model, TDD'd

--- yelp_clone/spec/features/restaurant_feature_spec.rb
+++ yelp_clone/spec/features/restaurant_feature_spec.rb
@@ -31,4 +31,29 @@ feature 'Restaurants' do
       expect(current_path).to eq("/restaurants/#{cafe_rouge.id}")
     end
   end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create(name: 'Cafe Rouge') }
+
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit Cafe Rouge'
+      fill_in 'Name', with: 'Cafe Rouge Restaurant'
+      click_button 'Update Restaurant'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
+
+  context 'deleting restaurants' do
+
+    before {Restaurant.create name: 'KFC'}
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
